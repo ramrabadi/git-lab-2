7935345fd6a629e870a4cf46261163ebe50afc81
Ramez <ra579529@ohio.edu>
2020-05-14 23:58:01 -0400

    Added git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Ramez Al-Rabadi>
+ *        Date: <5-14-2020>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+}// main
